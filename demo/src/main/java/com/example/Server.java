fe3dc342ed3a36adac57c5a6bfb0357f1a9af5ca
gitdev1 <gitdev1@gmail.com>
2025-01-15 20:00:24 +1300

    chat application

--- demo/src/main/java/com/example/Server.java
+++ demo/src/main/java/com/example/Server.java
@@ -7,23 +7,21 @@ import java.util.concurrent.ExecutionException;
 
 public class Server {
 	public static void main(String[] args) {
-		
+
 		int port = 8080;
 		try {
-				ServerSocket serverSocket = new ServerSocket(port);
-				System.out.println("Server is listening on port " + port);
-				while(true) {
-					Socket socket = serverSocket.accept();
-					System.out.println("Client Connect ...");
-					new ThreadServerController(socket).start();			
-				}
-			
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.out.println("Server is listening on port " + port);
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Client Connect ...");
+				new ThreadServerController(socket).start();
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
 
 }
-
